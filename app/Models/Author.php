318f7f3a4e975ce16abc30426e2ba999b4a093ff
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-07-03 01:19:33 -0400

    add implement automatic photo deletion on author delete and update by observer model

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Storage;
 
 class Author extends Model
 {
@@ -12,6 +13,30 @@ class Author extends Model
         'description',
         'photo'
     ];
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Automatically handle photo deletion when an author is deleted
+        static::deleting(function ($author) {
+            if ($author->photo && Storage::exists($author->photo)) {
+                Storage::delete($author->photo);
+            }
+        });
+
+        // Automatically handle photo deletion when an author is updated
+        static::updating(function ($author) {
+            if ($author->isDirty('photo')) {
+                $originalPhoto = $author->getOriginal('photo');
+
+                if ($originalPhoto && Storage::exists($originalPhoto)) {
+                    Storage::delete($originalPhoto);
+                }
+            }
+        });
+    }
 
     public function books(): HasMany
     {
