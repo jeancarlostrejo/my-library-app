e25257654a669be2131bb28f5678a79cd06011fc
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-07-03 01:43:58 -0400

    feat: add Genre resource with crud functionality

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    protected $fillable = ['name', 'description'];
+    protected $fillable = [
+        'name',
+        'description',
+        'slug'
+    ];
 
     public function books()
     {
