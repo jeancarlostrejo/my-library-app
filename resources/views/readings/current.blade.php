799c8ae747d04994e04382fbf8f8f592ce5ce485
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-07-04 01:30:25 -0400

    fix: handle null values for book title and author in current reading view

--- resources/views/readings/current.blade.php
+++ resources/views/readings/current.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-slot name="title">{{ $book->title }} por {{ $book->author->name }} | Mi Biblioteca</x-slot>
+        <x-slot name="title">{{ $book?->title ?? 'Sin lectura actual' }} por {{ $book?->author->name ?? "Jean Carlos Trejo" }} | Mi Biblioteca</x-slot>
 
     <x-book-show :book="$book" legend="🌟 Estoy leyendo 🌟"/>
 
