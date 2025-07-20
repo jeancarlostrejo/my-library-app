b2bc7b134a5d2a1021b58ade397b135e089366c0
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-07-20 10:37:20 -0400

    fix: styles to filter books

--- resources/views/livewire/filter-books.blade.php
+++ resources/views/livewire/filter-books.blade.php
@@ -1,7 +1,7 @@
 <div class="dark:text-gray-100 text-gray-900">
-    <div class="max-w-7xl mx-auto">
+    <div class="max-w-7xl mx-auto mb-5">
         <form wire:submit='dataSearch'>
-            <div class="md:grid md:grid-cols-3 gap-5 items-center">
+            <div class="grid md:grid-cols-3 gap-5 items-center">
                 <div class="mb-5 px-2 lg:px-0">
                     <label class="block mb-1 text-sm font-bold text-gray-900 dark:text-gray-100" for="author"> Buscar
                         por autor
@@ -17,9 +17,9 @@
                         class="rounded-md shadow-sm border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 w-full p-2 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100" />
                 </div>
 
-                <div class="my-5 px-2 lg:px-0 flex items-center">
+                <div class="lg:px-0">
                     <input type="submit"
-                        class="bg-blue-600 hover:bg-blue-700 transition-colors text-white text-sm font-bold px-10 py-2 rounded cursor-pointer uppercase w-full md:w-auto"
+                        class="bg-blue-600 hover:bg-blue-700 transition-colors text-white text-sm font-bold px-10 py-2 rounded cursor-pointer uppercase w-full md:w-auto p-2"
                         value="Buscar" />
                 </div>
             </div>
